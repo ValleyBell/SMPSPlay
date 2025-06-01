7cf78e12d0a70d4545e3e5b2a7f37b25c88886b4
Liu Wenyuan <15816141883@163.com>
2025-06-02 00:26:33 +0800

    Add scripts for VC2022

---
+++ libs/build_libs_VC2022-Win64.bat
@@ -0,0 +1,42 @@
+@echo off
+
+set BUILD_DIR=build_VC2022_Win64
+set BASE_DIR=%CD%\download
+set INST_DIR=%CD%\install
+set GENERATOR=Visual Studio 17 2022
+set ARCH=x64
+
+pushd .
+
+rem create clean folder structure
+rmdir /S /Q "%BUILD_DIR%"
+mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%"
+
+
+rem ---- build/install libvgm ---
+set LIBVGM_DIR=libvgm
+mkdir %LIBVGM_DIR%
+cd %LIBVGM_DIR%
+
+set LIBVGM_CMAKE_OPTS=-D BUILD_TESTS=OFF -D BUILD_PLAYER=OFF -D BUILD_VGM2WAV=OFF -D UTIL_CHARSET_CONV=OFF -D UTIL_LOADERS=OFF -D BUILD_LIBAUDIO=ON -D BUILD_LIBEMU=ON -D BUILD_LIBPLAYER=OFF -D SNDEMU__ALL=OFF -D SNDEMU_YM2612_GPGX=ON -D SNDEMU_SN76496_MAME=ON -D SNDEMU_UPD7759_ALL=ON
+cmake -G "%GENERATOR%" -A "%ARCH%" -D CMAKE_INSTALL_PREFIX="%INST_DIR%" %LIBVGM_CMAKE_OPTS% "%BASE_DIR%\%LIBVGM_DIR%"
+if errorlevel 1 goto builderror
+cmake --build . --config Debug --target INSTALL
+if errorlevel 1 goto builderror
+cmake --build . --config Release --target INSTALL
+if errorlevel 1 goto builderror
+
+cd ..\..\
+
+
+popd
+echo Done.
+
+exit /b
+
+:builderror
+popd
+@echo An error occoured!
+pause
+exit /b
